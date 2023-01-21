5be82561d41498a6562b7de99741eed94a2c75c7
chooomedia <9967192+chooomedia@users.noreply.github.com>
2023-01-21 16:24:33 +0100

    finalk

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
-function tooly_enqueue_styles() { 
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_script( 'injection-script', get_stylesheet_directory_uri() . '/assets/js/divi-animated-pixi-background.js','', '1.0', true );
-    wp_enqueue_script( 'pixi-animation', get_stylesheet_directory_uri() . '/assets/js/pixi.js','', '7.1.0', true );
-}
-add_action( 'wp_enqueue_scripts', 'tooly_enqueue_styles' );
+	add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+	function theme_enqueue_styles() {
+	    wp_enqueue_style( 'divi', get_template_directory_uri() . '/style.css' );
+      wp_enqueue_script( 'divi', plugin_dir_url( __FILE__ ) . 'js/scripts.js', array( 'jquery', 'divi-custom-script' ), '0.1.2', true );
+	}
+?>
